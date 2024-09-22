d7b238ac1e9555c6e29beb2d77c98037cd368d07
Hiromu OCHIAI <otiai10@gmail.com>
2024-09-22 19:26:54 +0900

    Fix demo to embed source code

--- demo/src/LineChart.tsx
+++ demo/src/LineChart.tsx
@@ -4,7 +4,7 @@ import { GoogleChart } from 'google-charts-react-component';
 export function MyLineChartComponent() {
     return (
         <div>
-            <h2 id="line-chart">Line Chart</h2>
+            <h1 id="line-chart">Line Chart</h1>
             <div>
                 <GoogleChart<google.visualization.LineChart>
                     type="line"
@@ -12,10 +12,20 @@ export function MyLineChartComponent() {
                     options={{}}
                 />
             </div>
+            {/*embed:ignore:start*/}
+            <div>
+                <pre>
+                    <code
+                        data-src="https://raw.githubusercontent.com/otiai10/google-charts-react-component/refs/heads/main/demo/src/LineChart.tsx"
+                        className="language-typescript"
+                    ></code></pre>
+            </div>
+            {/*embed:ignore:end*/}
         </div>
     );
 }
 
+//embed:ignore:start
 const data = [
     ["Year", "Sales", "Expenses"],
     ["2004", 1000, 400],
@@ -23,3 +33,4 @@ const data = [
     ["2006", 660, 1120],
     ["2007", 1030, 540],
 ];
+//embed:ignore:end
--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <div>
       <h1>Google Charts React Component</h1>
+      <hr />
       <MyLineChartComponent />
     </div>
   );
@@ -15,8 +16,34 @@ function App() {
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<React.StrictMode><App /></React.StrictMode>);
+
+/**
+ * Render source code for demo blocks
+ */
+/// <reference types="highlight.js" />
+declare const hljs: any;
+setTimeout(() => {
+  const codeblocks = document.querySelectorAll('pre>code[data-src]');
+  codeblocks.forEach(async (codeblock) => {
+    let src = codeblock.getAttribute('data-src');
+    if (!src) return;
+    const res = await fetch(src);
+    const text = await res.text();
+    let ignore = false;
+    const lines = text.split('\n').map(line => {
+      if (ignore) return null;
+      if (line.includes('embed:ignore:start')) {
+        ignore = true;
+        return null;
+      }
+      if (line.includes('embed:ignore:end')) {
+        ignore = false;
+        return null;
+      }
+      return line;
+    }).filter(line => line !== null);
+    codeblock.textContent = lines.join('\n');
+  });
+  setTimeout(() => hljs.highlightAll(), 1000);
+}, 0);
